2cd2d4cba8a8a3dd343c17694cadab7d8cca8abc
anlemadadeqie <512729961@qq.com>
2015-08-04 17:47:41 +0800

    创建和使用线程的私有数据

---
+++ tsd.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+
+pthread_key_t	key;
+
+void *thread2 (void *arg)
+{
+	int	tsd = 5;
+	printf("thread %u is running\n", (unsigned int)pthread_self());
+	pthread_setspecific (key, (void *) (&tsd));
+	printf("thread %u returns %d\n", (unsigned int)pthread_self(),* ((int *)pthread_getspecific(key)));
+}
+void *thread1(void *arg)
+{
+	int tsd = 0;
+	pthread_t thid2;
+
+	printf("thread %u is running\n",(unsigned int) pthread_self());
+	pthread_setspecific (key, (void *)(&tsd));
+	pthread_create (&thid2, NULL, thread2, NULL);
+	sleep(3);
+	printf("thread %u returns %d\n",(unsigned int) pthread_self(),*((int *) pthread_getspecific(key)));
+}
+int main(void)
+{
+	pthread_t thid1;
+	printf("main thread begins running\n");
+	pthread_key_create (&key, NULL);
+	pthread_create (&thid1, NULL, thread1, NULL);
+	sleep(10);
+	pthread_key_delete (key);
+	printf("main thread exit\n");
+	return 0;
+}
